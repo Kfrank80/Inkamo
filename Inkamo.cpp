000964acb3a31342eff15845e66c1fd119c127b7
Frank E. Ramirez <90592225+Kfrank80@users.noreply.github.com>
2024-01-24 14:56:38 -0400

    Add files via upload

---
+++ Inkamo.cpp
@@ -0,0 +1,30 @@
+#include "Inkamo.h"
+
+
+// Comprueba si hay actualizaciones. En caso de que las haya, 
+// la descarga a la carpeta actual sobreescribiendose a si mismo. 
+// Entonces, retorna true. Si no hay actualizaciones retorna false.
+bool Inkamo::actualizar()
+{
+    // TODO: Agregar aquí el código de implementación.
+    return false;
+}
+
+
+// Detecta impresoras, construye e inicializa cada cobjeto Printer 
+// llamando a las funciones correspondientes. 
+// Y luego agrega cada impresora al vector Impresoras.
+bool Inkamo::detectar_impresoras()
+{
+    // TODO: Agregar aquí el código de implementación.
+    return false;
+}
+
+
+// Valida el serial dado en el servidor para la operación requerida.
+bool Inkamo::validar_serial(std::string serial, std::string usuario, 
+                            std::string modelo_impresora, std::string hardwareID)
+{
+    // TODO: Agregar aquí el código de implementación.
+    return false;
+}
